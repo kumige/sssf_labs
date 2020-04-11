92dd176020238d5817f431e47d9ee049c7d4c34a
Mikko <mikko1.romo@gmail.com>
2020-04-11 14:14:04 +0300

    fix

--- production.js
+++ production.js
@@ -1,22 +1,18 @@
 'use strict'
 
-app.enable('trust proxy')
+module.exports = (app, httpPort) => {
+  app.enable('trust proxy');
 
-// Add a handler to inspect the req.secure flag (see
-// https://expressjs.com/en/api.html#req.secure). This allows us
-// to know whether the request was via http or https.
-// https://github.com/aerwin/https-redirect-demo/blob/master/server.js
-app.use ((req, res, next) => {
+  app.use ((req, res, next) => {
     if (req.secure) {
       // request was via https, so do no special handling
       next();
     } else {
+      console.log('http?, redirect to https...');
       // request was via http, so redirect to https
-      res.redirect('https://' + req.headers.host + req.url);
+      res.redirect(`https://${req.headers.host}${req.url}`);
     }
   });
-  
-  module.exports=(app, httpPort) => {
-    app.listen(httpPort);
 
-  }
+  app.listen(httpPort, () => console.log(`app listening on port ${httpPort}`));
+};
